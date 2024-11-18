e81c82eea9fc00b293b2386bffe0e4aa447aa5bf
Tambacu <ivanmatheus29@gmail.com>
2024-11-18 01:05:23 -0400

    Adicionando CORS

--- uni_back/app.py
+++ uni_back/app.py
@@ -1,11 +1,23 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from uni_back.routers import auth, feed, users
 from uni_back.schemas import Message
 
 app = FastAPI()
+origins = [
+    "http://localhost:8081/",
+
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[''],
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(auth.router)
